a1374936b9c61800491df32342b073ed4f7444db
Phuc Le <le.phuc.h@gmail.com>
2017-05-07 16:42:52 -0700

    Initial commit.

---
+++ app/utils/api.js
@@ -0,0 +1,69 @@
+var axios = require('axios');
+
+var id = "YOUR_CLIENT_ID";
+var sec = "secret"
+var params = "?client_id=" + id + "&client_secret=" + sec;
+
+function getProfile(username) {
+  return axios.get('https://api.github.com/users/' + username)
+    .then((user) => {
+      return user.data;
+    });
+}
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + '&per_page=100');
+}
+
+function getStarCount(repos) {
+  return repos.data.reduce((count, repo) => {
+    return count + repo.stargazers_count;
+  }, 0)
+}
+
+function calculateScore(profile, repos) {
+  let followers = profile.followers;
+  let totalStars = getStarCount(repos);
+
+  return (followers * 3 + totalStars);
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then((data)=> {
+    let profile = data[0];
+    let repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  });
+}
+
+function sortPlayers(players) {
+  return players.sort((a,b) => {
+    return b.score - a.score;
+  });
+}
+
+module.exports = {
+  battle: function(players) {
+    return axios.all(players.map(getUserData))
+  },
+  fetchPopularRepos: function(language) {
+    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+
+    return axios.get(encodedURI)
+      .then(function(response) {
+        return response.data.items;
+      })
+  }
+}
