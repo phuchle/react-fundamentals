33c173e9ace17c9cd534ba7804702d515e39695e
Phuc Le <le.phuc.h@gmail.com>
2017-05-07 19:46:55 -0700

    Add files I missed, facepalm.

---
+++ app/components/Loading.js
@@ -0,0 +1,60 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+
+var styles = {
+  content: {
+    textAlign: 'center',
+    fontSize: '35px'
+  }
+}
+
+class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: this.props.text
+    };
+  }
+  componentDidMount() {
+    var stopper = this.props.text + '...';
+
+    this.interval = setInterval(() => {
+      if(this.state.text === stopper) {
+        this.setState(() => {
+          return {
+            text: this.props.text
+          }
+        })
+      } else {
+        this.setState((prevState) => {
+          return {
+            text: prevState.text + '.'
+          }
+        });
+      }
+    }, this.props.speed);
+  }
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <p style={styles.content}>
+        {this.state.text}
+      </p>
+    )
+  }
+}
+
+Loading.PropTypes = {
+  text: PropTypes.string.isRequired,
+  speed: PropTypes.number.isRequired
+};
+
+Loading.defaultProps = {
+  text: 'Loading',
+  speed: 300
+};
+
+module.exports = Loading;
